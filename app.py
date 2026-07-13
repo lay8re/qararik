6bebe7df308b62b1f928e4d3541708c04c19f122
jory-alkhathami <joryf190@gmail.com>
2026-07-14 02:06:36 +0300

    Update app.py

--- app.py
+++ app.py
@@ -29,3 +29,7 @@ def analyze():
     result["recommendation"] = generate_ai_recommendation(result)
 
     return jsonify(result)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
